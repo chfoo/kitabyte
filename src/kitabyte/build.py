f7711c34a137124e67d6b63696bed09693514ce9
Christopher Foo <chris.foo@gmail.com>
2013-01-04 16:57:30 -0500

    Refactors builder. Adds experimental auto bold and oblique which needs impovement.

--- src/kitabyte/build.py
+++ src/kitabyte/build.py
@@ -6,192 +6,202 @@
 from kitabyte.font import Glyph
 from kitabyte.reader import Reader
 import argparse
+import collections
 import fontforge
 import kitabyte.reader
 import logging
+import math
 import os.path
+import psMat
 
 
 _logger = logging.getLogger(__name__)
 VISIBLE_CHARS = (u'x', u't', u'p')
 
 
-def make_glyph(font, glyph_def):
-    glyph = font.createChar(glyph_def.char_code)
-    glyph.manualHints = True
+class Builder(object):
+    GlyphSizes = collections.namedtuple('GlyphSizes', [
+        'square_size', 'descent_offset', 'row_len', 'row_count_flip',
+    ])
 
-    for arg in glyph_def.args:
-        if arg.startswith(u'reference:'):
-            code = int(arg.split(u':', 1)[1].lstrip('uU+'), 16)
-            glyph.addReference(fontforge.nameFromUnicode(code))
-        elif arg.startswith(u'diacritic:'):
-            code = int(arg.split(u':', 1)[1].lstrip('uU+'), 16)
-            glyph.appendAccent(fontforge.nameFromUnicode(code))
+    def make_glyph(self, glyph_def):
+        font = self.font
+        glyph = font.createChar(glyph_def.char_code)
+        glyph.manualHints = True
 
-    draw_glyph_rows(font, glyph, glyph_def)
-    add_anchors(font, glyph, glyph_def)
-    add_row_hints(font, glyph, glyph_def)
-    add_col_hints(font, glyph, glyph_def)
+        for arg in glyph_def.args:
+            if arg.startswith(u'reference:'):
+                code = int(arg.split(u':', 1)[1].lstrip('uU+'), 16)
+                glyph.addReference(fontforge.nameFromUnicode(code))
+            elif arg.startswith(u'diacritic:'):
+                code = int(arg.split(u':', 1)[1].lstrip('uU+'), 16)
+                glyph.appendAccent(fontforge.nameFromUnicode(code))
 
-    if u'combining' in glyph_def.args:
-        glyph.width = 0
-    else:
-        glyph.width = 16
+        glyph_sizes = self.calc_glyph_sizes(glyph_def)
 
+        self.draw_glyph_rows(glyph, glyph_def, glyph_sizes)
+        self.add_anchors(glyph, glyph_def, glyph_sizes)
+        self.add_row_hints(glyph, glyph_def, glyph_sizes)
+        self.add_col_hints(glyph, glyph_def, glyph_sizes)
 
-def draw_glyph_rows(font, glyph, glyph_def):
-    square_size = 2
-    descent_offset = font.descent + square_size
+        if u'combining' in glyph_def.args:
+            glyph.width = 0
+        else:
+            glyph.width = 16
 
-    pen = glyph.glyphPen()
-    row_len = len(glyph_def.bitmap)
+    def calc_glyph_sizes(self, glyph_def):
+        square_size = 2
+        descent_offset = self.font.descent + square_size
+        row_len = len(glyph_def.bitmap)
+        row_count_flip = 16
 
-    for row in xrange(row_len):
-#        row_flip = row_len - row
-        row_flip = 16 - row
+        return self.GlyphSizes(square_size, descent_offset, row_len, row_count_flip)
 
-        for col in xrange(len(glyph_def.bitmap[row])):
-            if glyph_def.bitmap[row][col] not in VISIBLE_CHARS:
-                continue
+    def draw_glyph_rows(self, glyph, glyph_def, glyph_sizes):
+        pen = glyph.glyphPen()
 
-            if u'combining' in glyph_def.args:
-                col -= 8
+        for row in xrange(glyph_sizes.row_len):
+            row_flip = glyph_sizes.row_count_flip - row
 
-            draw_square(pen, row_flip, col, square_size, descent_offset)
-
-
-def add_anchors(font, glyph, glyph_def):
-    row_len = len(glyph_def.bitmap)
-
-    for row in xrange(row_len):
-#        row_flip = row_len - row
-        row_flip = 16 - row
-
-        for col in xrange(len(glyph_def.bitmap[row])):
-            s = glyph_def.bitmap[row][col]
-
-            if s.lower() == u't':
-                glyph.addAnchorPoint('Top', 'base', col, row_flip)
-            elif s.lower() == u'p':
-                glyph.addAnchorPoint('Top', 'mark', col, row_flip)
-
-
-def add_row_hints(font, glyph, glyph_def):
-    square_size = 2
-    descent_offset = font.descent + square_size
-    row_len = len(glyph_def.bitmap)
-    l = []
-
-    for row in xrange(row_len):
-#        row_flip = row_len - row
-        row_flip = 16 - row
-
-        row_count = 0
-
-        for col in xrange(len(glyph_def.bitmap[row])):
-            if glyph_def.bitmap[row][col] not in VISIBLE_CHARS:
-                row_count = 0
-                continue
-
-            row_count += 1
-
-            if row_count == 3:
-                y = row_flip * square_size - descent_offset
-                l.append((y, square_size))
-
-    glyph.hhints = tuple(reversed(l))
-
-
-def add_col_hints(font, glyph, glyph_def):
-    square_size = 2
-    row_len = len(glyph_def.bitmap)
-    l = []
-
-    if not glyph_def.bitmap:
-        return
-
-    for col in xrange(max(len(row) for row in glyph_def.bitmap)):
-        col_count = 0
-
-        for row in xrange(row_len):
-            try:
+            for col in xrange(len(glyph_def.bitmap[row])):
                 if glyph_def.bitmap[row][col] not in VISIBLE_CHARS:
+                    continue
+
+                if u'combining' in glyph_def.args:
+                    col -= 8
+
+                self.draw_square(pen, row_flip, col, glyph_sizes)
+
+    def add_anchors(self, glyph, glyph_def, glyph_sizes):
+
+        for row in xrange(glyph_sizes.row_len):
+            row_flip = glyph_sizes.row_count_flip - row
+
+            for col in xrange(len(glyph_def.bitmap[row])):
+                s = glyph_def.bitmap[row][col]
+
+                if s.lower() == u't':
+                    glyph.addAnchorPoint('Top', 'base', col, row_flip)
+                elif s.lower() == u'p':
+                    glyph.addAnchorPoint('Top', 'mark', col, row_flip)
+
+    def add_row_hints(self, glyph, glyph_def, glyph_sizes):
+        l = []
+
+        for row in xrange(glyph_sizes.row_len):
+            row_flip = glyph_sizes.row_count_flip - row
+
+            row_count = 0
+
+            for col in xrange(len(glyph_def.bitmap[row])):
+                if glyph_def.bitmap[row][col] not in VISIBLE_CHARS:
+                    row_count = 0
+                    continue
+
+                row_count += 1
+
+                if row_count == 3:
+                    y = row_flip * glyph_sizes.square_size - \
+                        glyph_sizes.descent_offset
+                    l.append((y, glyph_sizes.square_size))
+
+        glyph.hhints = tuple(reversed(l))
+
+    def add_col_hints(self, glyph, glyph_def, glyph_sizes):
+        l = []
+
+        if not glyph_def.bitmap:
+            return
+
+        for col in xrange(max(len(row) for row in glyph_def.bitmap)):
+            col_count = 0
+
+            for row in xrange(glyph_sizes.row_len):
+                try:
+                    if glyph_def.bitmap[row][col] not in VISIBLE_CHARS:
+                        col_count = 0
+                        continue
+                except IndexError:
                     col_count = 0
                     continue
-            except IndexError:
-                col_count = 0
-                continue
 
-            col_count += 1
+                col_count += 1
 
-            if col_count == 3:
-                x = col * square_size
-                l.append((x, square_size))
+                if col_count == 3:
+                    x = col * glyph_sizes.square_size
+                    l.append((x, glyph_sizes.square_size))
 
-    glyph.vhints = l
+        glyph.vhints = l
 
+    def draw_square(self, pen, row, col, glyph_sizes):
+        square_size = glyph_sizes.square_size
+        descent_offset = glyph_sizes.descent_offset
+        x = col * square_size
+        y = row * square_size - descent_offset
 
-def draw_square(pen, row, col, square_size, descent_offset):
-    pen.moveTo((col * square_size,
-        row * square_size - descent_offset))
-    pen.lineTo((col * square_size + square_size,
-        row * square_size - descent_offset))
-    pen.lineTo((col * square_size + square_size,
-        row * square_size + square_size - descent_offset))
-    pen.lineTo((col * square_size,
-        row * square_size + square_size - descent_offset))
-    pen.closePath()
+        if 'bold' in self.font.fontname.lower():
+            x -= square_size * 0.5
+            square_size *= 1.5
 
+        pen.moveTo((x, y))
+        pen.lineTo((x + square_size, y))
+        pen.lineTo((x + square_size, y + square_size))
+        pen.lineTo((x, y + square_size))
+        pen.closePath()
 
-def build_font(dir_name, familyname, fontname, fullname):
-    font = fontforge.font()
-#    height = 32
-    font.descent = 8
-    font.ascent = 16
-#    font.em = 28
-#    font.design_size = 12
-    font.upos = -4
-    font.uwidth = 2
-    font.fontname = fontname
-    font.familyname = familyname
-    font.fullname = fullname
-    font.encoding = 'unicode'
+    def build_font(self, dir_name, familyname, fontname, fullname):
+        self.font = font = fontforge.font()
+    #    height = 32
+        font.descent = 8
+        font.ascent = 16
+    #    font.em = 28
+    #    font.design_size = 12
+        font.upos = -4
+        font.uwidth = 2
+        font.fontname = fontname
+        font.familyname = familyname
+        font.fullname = fullname
+        font.encoding = 'unicode'
 
-    font.addLookup('Anchors', 'gpos_mark2base', (), (
-        ("mark", (("DFLT", ("dflt")),)),
-    ))
-    font.addLookupSubtable('Anchors', 'DiacriticTop')
-    font.addAnchorClass('DiacriticTop', 'Top')
-#    font.addLookupSubtable('Anchors', 'DiacriticBottom')
-#    font.addAnchorClass('DiacriticBottom', 'Bottom')
+        font.addLookup('Anchors', 'gpos_mark2base', (), (
+            ("mark", (("DFLT", ("dflt")),)),
+        ))
+        font.addLookupSubtable('Anchors', 'DiacriticTop')
+        font.addAnchorClass('DiacriticTop', 'Top')
+    #    font.addLookupSubtable('Anchors', 'DiacriticBottom')
+    #    font.addAnchorClass('DiacriticBottom', 'Bottom')
 
-    reader = Reader(*sorted(kitabyte.reader.get_font_def_filenames(dir_name)))
+        reader = Reader(
+            *sorted(kitabyte.reader.get_font_def_filenames(dir_name)))
 
-    deferred_glyph_defs = []
+        deferred_glyph_defs = []
 
-    for glyph_def in kitabyte.reader.read_font_def(reader):
-        if isinstance(glyph_def, Glyph):
-            try:
-                _logger.debug('Processing u+%x %s', glyph_def.char_code,
+        for glyph_def in kitabyte.reader.read_font_def(reader):
+            if isinstance(glyph_def, Glyph):
+                try:
+                    _logger.debug('Processing u+%x %s', glyph_def.char_code,
+                        fontforge.nameFromUnicode(glyph_def.char_code))
+                    self.make_glyph(glyph_def)
+                except:
+                    _logger.exception('Deferring glyph u+%x %s',
+                        glyph_def.char_code,
+                        fontforge.nameFromUnicode(glyph_def.char_code))
+                    deferred_glyph_defs.append(glyph_def)
+
+        for glyph_def in deferred_glyph_defs:
+            _logger.debug('Processing u+%x %s', glyph_def.char_code,
                     fontforge.nameFromUnicode(glyph_def.char_code))
-                make_glyph(font, glyph_def)
-            except:
-                _logger.exception('Deferring glyph u+%x %s',
-                    glyph_def.char_code,
-                    fontforge.nameFromUnicode(glyph_def.char_code))
-                deferred_glyph_defs.append(glyph_def)
+            self.make_glyph(glyph_def)
 
-    for glyph_def in deferred_glyph_defs:
-        _logger.debug('Processing u+%x %s', glyph_def.char_code,
-                fontforge.nameFromUnicode(glyph_def.char_code))
-        make_glyph(font, glyph_def)
+        font.selection.all()
+        font.removeOverlap()
+        font.simplify()
+        font.correctDirection()
 
-    font.selection.all()
-    font.removeOverlap()
-    font.simplify()
-    font.correctDirection()
+        if 'oblique' in font.fontname.lower():
+            font.transform(psMat.skew(math.pi / 180 * 10))
 
-    return font
+        return font
 
 
 copyright_str = (u'Copyright © 2012-2013 by Christopher Foo '
@@ -204,10 +214,18 @@ def build_all(dest_dir_name, file_extensions=(u'sfd',)):
     names = [
         (u'regular', u'Kitabyte', u'Kitabyte-Regular', u'Kitabyte',
             kitabyte.__version__),
+        (u'regular', u'Kitabyte', u'Kitabyte-Bold', u'Kitabyte Bold',
+            kitabyte.__version__),
+        (u'regular', u'Kitabyte', u'Kitabyte-Oblique', u'Kitabyte Oblique',
+            kitabyte.__version__),
+        (u'regular', u'Kitabyte', u'Kitabyte-BoldOblique',
+            u'Kitabyte Bold Oblique', kitabyte.__version__),
     ]
 
+    builder = Builder()
+
     for dir_name, familyname, fontname, fullname, version in names:
-        font = build_font(dir_name, familyname, fontname, fullname)
+        font = builder.build_font(dir_name, familyname, fontname, fullname)
         font.version = version
         font.copyright = copyright_str
 
