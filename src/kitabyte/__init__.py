6844f72dd3456ba42b26244bae4d53d573f7936d
Christopher Foo <chris.foo@gmail.com>
2013-02-19 23:19:42 -0500

    Removes mark on comma which gave it an incorrect combining behavior. Fixes wrong diacritics on u+0150/u+0170. Fill up combining diacritics with existing refs.

--- src/kitabyte/__init__.py
+++ src/kitabyte/__init__.py
@@ -6,6 +6,6 @@
 import distutils.version
 
 short_version = u'1.8'  # N.N
-__version__ = short_version + u'.4'  # N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]
+__version__ = short_version + u'.5'  # N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]
 
 distutils.version.StrictVersion(__version__)
